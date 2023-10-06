93865caec22a5c785282a1535c8309450506ef5b
AlexCernas2901 <acernas1@ucol.mx>
2023-10-05 23:59:52 -0600

    database connection

--- app/src/main/java/com/example/listalumnos/MainActivity.kt
+++ app/src/main/java/com/example/listalumnos/MainActivity.kt
@@ -10,54 +10,87 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listalumnos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    // Vinculación de las vistas con MainActivity.kt
+    //Vinculacion de las vistas con MainActivity
     private lateinit var binding: ActivityMainBinding
-    // ArrayList de la clase Alumno
-    private val data = ArrayList<Alumno>()
-    // Referencia al adaptador
-    private lateinit var rvAdapter: AlumnoAdapter
+    // ArrayList of class ItemsViewModel
+    private var  data = ArrayList<Alumno>()
+    var idAlumno: Int = 0
+
+    private lateinit var rvAdapter : AlumnoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        val dbalumnos = DBHelperAlumno(this)
+        val db = dbalumnos.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM alumnos",null)
 
-        data.add(
-            Alumno(
-                "José Nabor",
-                "20102345",
-                "jmorfin@ucol.mx",
-                "https://imagenpng.com/wp-content/uploads/2017/02/pokemon-hulu-pikach.jpg"
-            )
-        )
-        data.add(
-            Alumno(
-                "Luis Antonio",
-                "20112345",
-                "jmorfin@ucol.mx",
-                "https://i.pinimg.com/236x/e0/b8/3e/e0b83e84afe193922892917ddea28109.jpg"
-            )
-        )
-        data.add(
-            Alumno(
-                "Juan Pedro",
-                "20122345",
-                "jmorfin@ucol.mx",
-                "https://i.pinimg.com/736x/9f/6e/fa/9f6efa277ddcc1e8cfd059f2c560ee53--clipart-gratis-vector-clipart.jpg"
-            )
-        )
+        if(cursor.moveToFirst()){
+            do {
+                idAlumno = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+                var itemNom = cursor.getString(cursor.getColumnIndexOrThrow("nombre"))
+                var itemCue = cursor.getString(cursor.getColumnIndexOrThrow("nocuenta"))
+                var itemCorr = cursor.getString(cursor.getColumnIndexOrThrow("email"))
+                var itemImg = cursor.getString(cursor.getColumnIndexOrThrow("imagen"))
 
-        rvAdapter = AlumnoAdapter(this, data, object : AlumnoAdapter.OptionsMenuClickListener {
-            override fun onOptionsMenuClicked(position: Int) {
-                itemOptionsMenu(position)
-            }
-        })
+                data.add(
+                    Alumno("${itemNom}",
+                        "${itemCue}",
+                        "${itemCorr}",
+                        "${itemImg}"
+                    )
+                )
 
-        binding.recyclerview.adapter = rvAdapter
+            } while (cursor.moveToNext())
+            db.close()
+            cursor.close()
 
-        //Variable para recibir extras
+            binding.recyclerview.layoutManager = LinearLayoutManager(this)
+
+            rvAdapter = AlumnoAdapter(this, data, object : AlumnoAdapter.OptionsMenuClickListener{
+                override fun onOptionsMenuClicked(position: Int) {
+                    itemOptionsMenu(position)
+                }
+            })
+            binding.recyclerview.adapter = rvAdapter
+        }
+
+//        data.add(
+//            Alumno (
+//                "${itemNom}",
+//                "${itemCue}",
+//                "${itemCorr}",
+//                "${itemImg}"
+//            )
+//        )
+//
+//        data.add(
+//            Alumno(
+//                "José Nabor",
+//                "20102345",
+//                "jmorfin@ucol.mx",
+//                "https://imagenpng.com/wp-content/uploads/2017/02/pokemon-hulu-pikach.jpg"
+//            )
+//        )
+//        data.add(
+//            Alumno(
+//                "Luis Antonio",
+//                "20112345",
+//                "jmorfin@ucol.mx",
+//                "https://i.pinimg.com/236x/e0/b8/3e/e0b83e84afe193922892917ddea28109.jpg"
+//            )
+//        )
+//        data.add(
+//            Alumno(
+//                "Juan Pedro",
+//                "20122345",
+//                "jmorfin@ucol.mx",
+//                "https://i.pinimg.com/736x/9f/6e/fa/9f6efa277ddcc1e8cfd059f2c560ee53--clipart-gratis-vector-clipart.jpg"
+//            )
+//        )
+
         val parExtra = intent.extras
         val msje = parExtra?.getString("mensaje")
         val nombre = parExtra?.getString("nombre")
@@ -66,7 +99,7 @@ class MainActivity : AppCompatActivity() {
         val image = parExtra?.getString("image")
 
         //Preguntamos se el mensaje es para nuevo alumno
-        if (msje=="nuevo"){
+        if (msje== "nuevo"){
             //Sacamos en una variable el total de elementos en nuestra lista
             val insertIndex: Int = data.count()
             //Usamos la variable insertIndex para indicar la posición del nuevo alumno
@@ -83,28 +116,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.faButton.setOnClickListener {
-            val intento1 = Intent(this, MainActivityNuevo::class.java)
+            val intento1 = Intent(this,MainActivityNuevo::class.java)
             startActivity(intento1)
         }
     }
 
     private fun itemOptionsMenu(position: Int) {
-        val popupMenu = PopupMenu(this, binding.recyclerview[position].findViewById(R.id.textViewOptions))
+        val popupMenu = PopupMenu(this,binding.recyclerview[position].findViewById(R.id.textViewOptions))
         popupMenu.inflate(R.menu.options_menu)
         //Para cambiarnos de activity
-        val intento2 = Intent(this, MainActivityNuevo::class.java)
+        val intento2 = Intent(this,MainActivityNuevo::class.java)
         //Implementar el click en el item
-        popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+        popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener{
             override fun onMenuItemClick(item: MenuItem?): Boolean {
-                when (item?.itemId) {
+                when(item?.itemId){
                     R.id.borrar -> {
                         val tmpAlum = data[position]
                         data.remove(tmpAlum)
                         rvAdapter.notifyDataSetChanged()
                         return true
                     }
-
-                    R.id.editar -> {
+                    R.id.editar ->{
                         //Tomamos los datos del alumno, en la posición de la lista donde hicieron click
                         val nombre = data[position].nombre
                         val cuenta = data[position].cuenta
@@ -112,13 +144,13 @@ class MainActivity : AppCompatActivity() {
                         val image = data[position].imagen
                         //En position tengo el indice del elemento en la lista
                         val idAlum: Int = position
-                        intento2.putExtra("mensaje", "edit")
-                        intento2.putExtra("nombre", "${nombre}")
-                        intento2.putExtra("cuenta", "${cuenta}")
-                        intento2.putExtra("correo", "${correo}")
-                        intento2.putExtra("image", "${image}")
+                        intento2.putExtra("mensaje","edit")
+                        intento2.putExtra("nombre","${nombre}")
+                        intento2.putExtra("cuenta","${cuenta}")
+                        intento2.putExtra("correo","${correo}")
+                        intento2.putExtra("image","${image}")
                         //Pasamos por extras el idAlum para poder saber cual editar de la lista (ArrayList)
-                        intento2.putExtra("idA", idAlum)
+                        intento2.putExtra("idA",idAlum)
                         startActivity(intento2)
                         return true
                     }
--- app/src/main/java/com/example/listalumnos/MainActivityNuevo.kt
+++ app/src/main/java/com/example/listalumnos/MainActivityNuevo.kt
@@ -1,8 +1,11 @@
 package com.example.listalumnos
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.contentValuesOf
 import com.example.listalumnos.MainActivity
 import com.example.listalumnos.databinding.ActivityMainNuevoBinding
 
@@ -10,10 +13,6 @@ class MainActivityNuevo : AppCompatActivity() {
     private lateinit var binding: ActivityMainNuevoBinding // Mueve la declaración aquí
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainNuevoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        //En el onCreate
         binding = ActivityMainNuevoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -25,14 +24,40 @@ class MainActivityNuevo : AppCompatActivity() {
             val txtCorr = binding.txtCorreo.text
             val txtImg = binding.txtImage.text
 
-            //Creamos el Intent para pasarnos al MainActivity y mandamos por extras los valores
+            val dbalumnos = DBHelperAlumno(this)
+
+            val db = dbalumnos.writableDatabase
+
+            val newReg = ContentValues()
+            newReg.put("nombre", txtNom.toString())
+            newReg.put("nocuenta", txtCue.toString())
+            newReg.put("email", txtCorr.toString())
+            newReg.put("imagen", txtImg.toString())
+
+            val res = db.insert("alumnos", null, newReg)
+            db.close()
+
+            if (res.toInt() == -1) {
+                Toast.makeText(this, "No se inserto el registro", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Registro insertado con éxito", Toast.LENGTH_LONG).show()
+                binding.txtNombre.text.clear()
+                binding.txtCuenta.text.clear()
+                binding.txtCorreo.text.clear()
+                binding.txtImage.text.clear()
+            }
+
+//            //Creamos el Intent para pasarnos al MainActivity y mandamos por extras los valores
+//            val intento2 = Intent(this,MainActivity::class.java)
+//            //usamos la etiqueta mensaje para indicar que es nuevo alumno
+//            intento2.putExtra("mensaje","nuevo")
+//            intento2.putExtra("nombre","${txtNom}")
+//            intento2.putExtra("cuenta","${txtCue}")
+//            intento2.putExtra("correo","${txtCorr}")
+//            intento2.putExtra("image","${txtImg}")
+//            startActivity(intento2)
+
             val intento2 = Intent(this,MainActivity::class.java)
-            //usamos la etiqueta mensaje para indicar que es nuevo alumno
-            intento2.putExtra("mensaje","nuevo")
-            intento2.putExtra("nombre","${txtNom}")
-            intento2.putExtra("cuenta","${txtCue}")
-            intento2.putExtra("correo","${txtCorr}")
-            intento2.putExtra("image","${txtImg}")
             startActivity(intento2)
         }
     }
